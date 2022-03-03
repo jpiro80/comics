775f166e06a2bf61dc5f410ece0b2ab2fa18c517
jpiro80 <85123589+jpiro80@users.noreply.github.com>
2022-03-03 11:09:56 -0300

    reordenando archivos y carpetas

---
+++ src/dao/comic_dao.py
@@ -0,0 +1,73 @@
+from conexion import Conexion
+from comic import Comic
+from logger_base import log
+
+class ComicDAO:
+    '''
+    DAO (Data Access Object)
+    CRUD (Create-Read-Update-Delete)
+    '''
+    _SELECCIONAR = 'SELECT * FROM comic ORDER BY comic_id'
+    # _INSERTAR = 'INSERT INTO comic(descripcion) VALUES(%s)'
+    # _ACTUALIZAR = 'UPDATE comic SET descripcion=%s WHERE comic_id=%s'
+    # _ELIMINAR = 'DELETE FROM comic WHERE comic_id=%s'
+
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion() as conexion:
+            with conexion.cursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                comics = []
+                for registro in registros:
+                    comic = Comic(registro[0], registro[1], registro[2], registro[3])
+                    comics.append(comic)
+                return comics
+
+    # @classmethod
+    # def insertar(cls, comic):
+    #     with Conexion.obtenerConexion() as conexion:
+    #         with conexion.cursor() as cursor:
+    #             valores = (comic.descripcion)
+    #             cursor.execute(cls._INSERTAR, valores)
+    #             log.debug(f'Comic insertado: {comic}')
+    #             return cursor.rowcount
+
+    # @classmethod
+    # def actualizar(cls, comic):
+    #     with Conexion.obtenerConexion():
+    #         with Conexion.obtenerCursor() as cursor:
+    #             valores = (comic.comic_id, comic.descripcion, comic.autor_id, comic.editorial_id)
+    #             cursor.execute(cls._ACTUALIZAR, valores)
+    #             log.debug(f'Comic actualizado: {comic}')
+    #             return cursor.rowcount
+
+    # @classmethod
+    # def eliminar(cls, comic):
+    #     with Conexion.obtenerConexion():
+    #         with Conexion.obtenerCursor() as cursor:
+    #             valores = (comic.comic_id,)
+    #             cursor.execute(cls._ELIMINAR, valores)
+    #             log.debug(f'Objeto eliminado: {comic}')
+    #             return cursor.rowcount
+
+if __name__ == '__main__':
+    # Insertar un registro
+    # comic1 = Comic(descripcion='Spiderman-1990')
+    # comics_insertados = ComicDAO.insertar(comic1)
+    # log.debug(f'Comics insertados: {comics_insertados}')
+
+    # Actualizar un registro
+    # comic1 = Comic(1, 'Spiderman-1990', 1, 1)
+    # comics_actualizados = ComicDAO.actualizar(comic1)
+    # log.debug(f'Comics actualizados: {comics_actualizados}')
+
+    # Eliminar un registro
+    # comic1 = Comic(comic_id=1)
+    # comics_eliminados = ComicDAO.eliminar(comic1)
+    # log.debug(f'Comics eliminados: {comics_eliminados}')
+
+    # Seleccionar objetos
+    comics = ComicDAO.seleccionar()
+    for comic in comics:
+        log.debug(comic)
---
+++ src/entities/comic.py
@@ -0,0 +1,55 @@
+from logger_base import log
+
+class Comic:
+    def __init__(self, comic_id=None, descripcion=None, autor_id=None, editorial_id=None):
+        self._comic_id = comic_id
+        self._descripcion = descripcion
+        self._autor_id = autor_id
+        self._editorial_id = editorial_id
+
+    def __str__(self):
+        return f'''
+            Comic Id: {self._comic_id}, Descripcion: {self._descripcion},
+            Autor Id: {self._autor_id}, Editorial Id: {self._editorial_id}
+        '''
+    @property
+    def comic_id(self):
+        return self._comic_id
+
+    @comic_id.setter
+    def comic_id(self, comic_id):
+        self._comic_id = comic_id
+
+    @property
+    def descripcion(self):
+        return self._descripcion
+
+    @descripcion.setter
+    def descripcion(self, descripcion):
+        self._descripcion = descripcion
+
+    @property
+    def autor_id(self):
+        return self._autor_id
+
+    @autor_id.setter
+    def autor_id(self, autor_id):
+        self._autor_id = autor_id
+
+    @property
+    def editorial_id(self):
+        return self._editorial_id
+
+    @editorial_id.setter
+    def editorial_id(self, editorial_id):
+        self._editorial_id = editorial_id
+
+if __name__ == '__main__':
+    comic1 = Comic(1, 'X-MEN #2 - 1995', 1, 1)
+    log.debug(comic1)
+    # Simular un insert
+    comic1 = Comic(descripcion='X-MEN #2 - 1995')
+    log.debug(comic1)
+    # Simular un delete
+    comic1 = Comic(comic_id=1)
+    log.debug(comic1)
